1e93de610a3420bc2952bc4985b64f838948bd89
Clint G <cgardner4@gmail.com>
2022-10-31 12:11:29 +0100

    project complete

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let operatorInProgress = false;
 let witchOperator = ''; //It's Halloween!
 let flashlightOn = false;
 
+//construct html elements
 const page = document.querySelector('html');
 const displayScreen = document.querySelector('.display');
 const allNumberButtons = document.querySelectorAll('.number');
@@ -12,11 +13,43 @@ const allOperatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.clear');
 const equalsButton = document.querySelector('.equals');
 const negativeSign = document.querySelector('.plus-minus');
-const charger = document.querySelector('.fa-sun');
+const charger = document.querySelector('.flashlight');
 const solarPanel = document.querySelector('.solar-panel');
+const deleteButton = document.querySelector('.delete');
 
-
-
+//Event Listeners
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+    }
+    if ((e.key >= 0 && e.key <= 9) || e.key === '.') {
+        if (operandInProgress.length >= 10) return;
+        if (e.key === '.' && (/\./.test(operandInProgress))) return;
+        if (operatorInProgress) {
+            operatorInProgress = false;
+            clearDisplay();
+        }
+        operandInProgress += e.key;
+        display(operandInProgress);
+    } else if (e.key === '*' || e.key === '+' || e.key === '-' || e.key === '/') {
+        setOperator(e.key);
+        if (operand1 === '') {
+            operand1 = operandInProgress;
+            operandInProgress = '';
+            operatorInProgress = true;
+            return;
+        } else {
+            calculateExpression(e.key);
+        }
+    } else if (e.key === 'Backspace') {
+        deleteBtn();
+    } else if (e.key === 'Escape') {
+        clearAll();
+    } else {
+        return;
+    }
+});
+deleteButton.addEventListener('click', deleteBtn);
 charger.addEventListener('mousedown', readyCharge);
 negativeSign.addEventListener('click', plusMinus);
 allOperatorButtons.forEach(btn => {
@@ -29,6 +62,15 @@ clearButton.addEventListener('click', clearAll);
 equalsButton.addEventListener('click', equalsButtonClick);
 solarPanel.addEventListener('mouseover', charging);
 solarPanel.addEventListener('mouseout', doneCharging);
+
+//calculator functions
+function deleteBtn() {
+    if (typeof (operandInProgress) !== 'string') {
+        operandInProgress = operandInProgress.toString();
+    }
+    operandInProgress = operandInProgress.slice(0, -1);
+    display(operandInProgress);
+}
 
 function doneCharging() {
     if (flashlightOn) {
@@ -48,6 +90,7 @@ function charging() {
     }
 }
 
+//change mouse pointer to flashlight to 'charge' device
 function readyCharge() {
     if (!flashlightOn) {
         flashlightOn = true;
@@ -64,13 +107,8 @@ function equalsButtonClick() {
     }
     calculateExpression(witchOperator);
     operandInProgress = operand1;
-    //formatNumber(operandInProgress);
     operand1 = '';
     operatorInProgress = false;
-    console.log('operand1: ' + operand1,
-        ' andIn: ' + operandInProgress,
-        ' atorIn: ' + operatorInProgress,
-        ' witch: ' + witchOperator);
 }
 
 function resetValues() {
@@ -81,7 +119,7 @@ function resetValues() {
 }
 
 function calculateExpression(e) {
-    if (e === 'plus') {
+    if (e === '+') {
         if (operandInProgress === '') {
             operandInProgress = 0;
         } else if (operand1 === '') {
@@ -90,10 +128,9 @@ function calculateExpression(e) {
         operand1 = Number(operand1);
         operandInProgress = Number(operandInProgress);
         operand1 = add(operand1, operandInProgress);
-        console.log(typeof (operand1));
         clearDisplay();
         display(formatNumber(operand1));
-    } else if (e === 'minus') {
+    } else if (e === '-') {
         if (operandInProgress === '') {
             operandInProgress = 0;
         } else if (operand1 === '') {
@@ -104,7 +141,7 @@ function calculateExpression(e) {
         operand1 = sub(operand1, operandInProgress);
         clearDisplay();
         display(formatNumber(operand1));
-    } else if (e === 'times') {
+    } else if (e === '*') {
         if (operandInProgress === '') {
             operandInProgress = 1;
         } else if (operand1 === '') {
@@ -113,17 +150,14 @@ function calculateExpression(e) {
         operand1 = Number(operand1);
         operandInProgress = Number(operandInProgress);
         operand1 = mult(operand1, operandInProgress);
-        console.log(operand1);
         clearDisplay();
-        ;
-        console.log(operand1);
         display(formatNumber(operand1));
-    } else if (e === 'divided') {
+    } else if (e === '/') {
         if (operandInProgress === '') {
             operandInProgress = 1;
         } else if (operand1 === '') {
             operand1 = 1;
-        } else if (operandInProgress = '0') {
+        } else if (operandInProgress === '0') {
             clearAll();
             display('ERROR!');
             return;
@@ -137,16 +171,6 @@ function calculateExpression(e) {
     operatorInProgress = true;
 }
 
-/*function keepTenDigits(ans) {
-    ans = toString(ans);
-    if (ans.length > 10) {
-        clearAll();
-        displayScreen.textContent = 'MEM ERROR!';
-        return;
-    }
-    return ans;
-}*/
-
 function setOperator(newOperator) {
     witchOperator = newOperator
 }
@@ -157,18 +181,10 @@ function operatorButtonClick(e) {
         operand1 = operandInProgress;
         operandInProgress = '';
         operatorInProgress = true;
-        console.log('operand1: ' + operand1,
-            ' andIn: ' + operandInProgress,
-            ' atorIn: ' + operatorInProgress,
-            ' witch: ' + witchOperator);
         return;
     } else {
         calculateExpression(e.target.value);
     }
-    console.log('operand1: ' + operand1,
-        ' andIn: ' + operandInProgress,
-        ' atorIn: ' + operatorInProgress,
-        ' witch: ' + witchOperator);
 }
 
 function plusMinus(e) {
@@ -179,26 +195,17 @@ function plusMinus(e) {
         operandInProgress = operandInProgress.substring(1);
         display(operandInProgress);
     }
-    console.log('operand1: ' + operand1,
-        ' andIn: ' + operandInProgress,
-        ' atorIn: ' + operatorInProgress,
-        ' witch: ' + witchOperator);
 }
-
 
 function NumberButtonClick(e) {
     if (operandInProgress.length >= 10) return;
-    if (e.target.value === '.' && operandInProgress % 1 != 0) return;
+    if (e.target.value === '.' && (/\./.test(operandInProgress))) return;
     if (operatorInProgress) {
         operatorInProgress = false;
         clearDisplay();
     }
     operandInProgress += e.target.value;
     display(operandInProgress);
-    console.log('operand1: ' + operand1,
-        ' andIn: ' + operandInProgress,
-        ' atorIn: ' + operatorInProgress,
-        ' witch: ' + witchOperator);
 }
 
 function clearDisplay() {
@@ -229,33 +236,26 @@ function divide(x, y) {
 function formatNumber(num) {
     if (typeof (num) != 'string') {
         num = num.toString();
-        console.log(num);
         if (num.length >= 11) {
             clearAll();
             num = 'MEM ERROR!'
         }
     }
-    console.log(num);
     return num;
 }
 
 function display(input) {
-    //formatNumber(input);
-    console.log(input);
     displayScreen.textContent = input;
-    //operandInProgress = input.substring(0,10);
-
 }
-
+/*Unused but can limit decimals
 Number.prototype.limitDecimals = function (n) {
     const d = Math.pow(10, n);
     return Math.round((this + Number.EPSILON) * d) / d;
 }
 
-//console.log(1234444444444.678999.limitDecimals(24), 1.5 % 1);
-//console.log(Number('1.88888'));
-//console.log('hello'.substring(1));
-console.log(formatNumber('1234567899999.1'));
-//console.log(mult(1111111111, 2222222222222));
-console.log(formatNumber(1234364564564));
-//console.log(display('123'));
+console log to test the main values
+console.log('operand1: ' + operand1,
+' andIn: ' + operandInProgress,
+' atorIn: ' + operatorInProgress,
+' witch: ' + witchOperator);
+*/
